1c1adcb5d59267af8970a9acda555ffd524a336b
Luke <lucusaurelius@gmail.com>
2018-11-08 22:07:09 -0500

    working compose file and working python

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from lib.db import conn
+
+if __name__ == '__main__':
+
+    cur = conn.cursor()
+
+    # Query the database and obtain data as Python objects.
+    cur.execute('SELECT * from pg_extension;')
+    results = cur.fetchall()
+    for result in results:
+        print(result)
+
+    # Close communication with the database.
+    cur.close()
+    conn.close()
